062515b2f4a9b5d854a1dcb2cf33c162ff044710
valentinadinnadedeiokai <valentinaokai@gmail.com>
2022-06-14 02:48:29 -0700

    commit tree bfb5596a413017a321b984ee972558ab4ebfbc40

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+
+
+function returnsANamedFunction(){
+    const callback = function(){};
+    return callback
+}
+
+
+function returnsAnAnonymousFunction(){
+    return function(){
+}
+}
